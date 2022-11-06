195f2cb1a42a8604399f0fdb097f85662db5b053
Grigroviska <karagozoglualper@protonmail.com>
2022-11-06 23:56:58 +0300

    Created
    The weather app HoWeather was created.

---
+++ app/src/main/java/com/gematriga/howeather/Utilities/ApiInterface.kt
@@ -0,0 +1,23 @@
+package com.gematriga.howeather.Utilities
+
+import com.gematriga.howeather.Models.WeatherModel
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiInterface {
+
+    @GET("weather")
+    fun getCurrentWeatherData(
+        @Query("lat") lat: String,
+        @Query("lon") lon: String,
+        @Query("APPID") appid:String
+    ): Call<WeatherModel>
+
+    @GET("weather")
+    fun getCityWeatherData(
+        @Query("q") q: String,
+        @Query("APPID") appid:String
+    ): Call<WeatherModel>
+
+}
